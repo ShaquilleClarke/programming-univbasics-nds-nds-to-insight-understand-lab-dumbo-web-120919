d44bec5f9b18531d605d7c10a6f13e6ee2378105
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-05 23:33:25 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   index = 0
 
   while index < ss_movies.length do
-    titles = ss_movies[index][:titles]
+    titles = ss_movies[index][:title]
     puts titles
     index += 1
   end
